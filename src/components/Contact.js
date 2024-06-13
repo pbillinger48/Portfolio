b32c57fd72890bfc026d4362a37f89848c9e0f66
parkerbillinger <pbillinger48@ksu.edu>
2024-06-12 21:15:24 -0500

    Contact changed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,27 +3,26 @@
 import React from "react";
 
 export default function Contact() {
-
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-        <img
-            src="./ResumePic.png"
-            alt="Resume"
-            width="100%"
-            height="100%"
-            className="absolute inset-0"
-            style={{ filter: "opacity(0.7)" }}
-          />
-          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden p-10 flex flex-row">
+          <div className="flex-shrink-0 mr-10">
+            <img
+              src="./ResumePic.png"
+              alt="Resume"
+              width="150px"
+              height="150px"
+              className="rounded-full"
+              style={{ filter: "opacity(0.9)" }}
+            />
+          </div>
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md flex-grow">
             <div className="lg:w-1/2 px-6">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 Location
               </h2>
-              <p className="mt-1">
-                Kansas City Metro Area
-              </p>
+              <p className="mt-1">Kansas City Metro Area</p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
@@ -31,7 +30,7 @@ export default function Contact() {
               </h2>
               <a className="text-indigo-400 leading-relaxed">
                 pbillinger48@gmail.com
-                </a>
+              </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
@@ -39,16 +38,16 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 RESUME
               </h2>
-              <a 
-                href="./ParkerBillingerResume.pdf" 
-                download="Parker_Billinger_Resume.pdf" 
+              <a
+                href="./ParkerBillingerResume.pdf"
+                download="Parker_Billinger_Resume.pdf"
                 className="text-indigo-400 leading-relaxed"
               >
                 Download Resume
               </a>
             </div>
           </div>
-        </div>       
+        </div>
       </div>
     </section>
   );
